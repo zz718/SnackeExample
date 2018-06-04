ceb8dd64316ca0a864e02dc3a2b3223ed5c08602
zz718 <37528121+zz718@users.noreply.github.com>
2018-06-04 10:13:08 +0300

    Add files via upload
    full snake

---
+++ function_main.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ncurses.h>
+
+#define vertical 40
+#define horizontal 200
+#define down 115
+#define up 119
+#define left 97
+#define right 100
+
+
+
+typedef struct Snake
+{
+    char symbol;
+    int size;
+    char direction;
+    char prev_direction;
+    int tail_X;
+    int tail_Y;
+    int head_X;
+    int head_Y;
+
+}snake;
+
+
+typedef struct snake_pos
+{
+    int Y[vertical*horizontal];
+    int X[vertical*horizontal];
+
+}snake_pos;
+
+
+
+typedef struct food
+{
+    int X;
+    int Y;
+    char symbol;
+
+}food;
+
+
+
+void snake_init(snake *snake1);
+void pos_init(snake_pos *pos1);
+void food_init(food *food1);
+void gotoxy(int,int);
+void snake_place(snake *snake1, snake_pos *pos1);
+void snake_move(snake *snake1, snake_pos *pos1, food *food1, int*);
+void move_tail(snake *snake1, snake_pos *pos1);
+void move_head(snake *snake1, snake_pos *pos1);
+void food_print(food *food1);
+int game_over(snake *snake1, snake_pos *pos1);
+void set_borders();
+void print_score(int*);
+
+int main()
+{
+      int snake_speed=100000;
+      int score=0;
+
+      /* struct init */
+
+      snake snake1;
+      snake_pos pos1;
+      food food1;
+      snake_init(&snake1);
+      pos_init(&pos1);
+      food_init(&food1);
+
+
+      /* set initial field */
+
+      system("clear");
+      system("stty -echo");
+      curs_set(0);                    // doesn't work for some reason
+      snake_place(&snake1,&pos1);
+      set_borders();
+      food_print(&food1);
+
+
+      /* use system call to make terminal send all keystrokes directly to stdin */
+
+      system ("/bin/stty raw");
+
+
+      /* while snake not got collided into itself */
+
+      while(!(game_over(&snake1,&pos1)))
+      {
+
+          /* while key not pressed */
+
+          while (!kbhit())
+          {
+                 usleep(snake_speed);
+                 snake_move(&snake1,&pos1,&food1,&score);
+                 if (game_over(&snake1,&pos1))
+                 {
+                     break;
+                 }
+
+          }
+          /* store previous direction and fetch a new one */
+
+          snake1.prev_direction=snake1.direction;
+          snake1.direction=getchar();
+
+
+
+     }
+      /* use system call to set terminal behaviour to more normal behaviour */
+      system ("/bin/stty cooked");
+      system("stty echo");
+      system("clear");
+      printf("\n\n Final score: %d \n\n", score);
+
+
+
+      return 0;
+
+}
+
---
+++ inicialization.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <ncurses.h>
+
+#define vertical 40
+#define horizontal 200
+#define down 115
+#define up 119
+#define left 97
+#define right 100
+
+
+
+typedef struct Snake
+{
+    char symbol;
+    int size;
+    char direction;
+    char prev_direction;
+    int tail_X;
+    int tail_Y;
+    int head_X;
+    int head_Y;
+
+}snake;
+
+
+typedef struct snake_pos
+{
+    int Y[vertical*horizontal];
+    int X[vertical*horizontal];
+
+}snake_pos;
+
+
+
+typedef struct food
+{
+    int X;
+    int Y;
+    char symbol;
+
+}food;
+
+
+
+void snake_init(snake *snake1);
+void pos_init(snake_pos *pos1);
+void food_init(food *food1);
+void gotoxy(int,int);
+void snake_place(snake *snake1, snake_pos *pos1);
+void snake_move(snake *snake1, snake_pos *pos1, food *food1, int*);
+void move_tail(snake *snake1, snake_pos *pos1);
+void move_head(snake *snake1, snake_pos *pos1);
+void food_print(food *food1);
+int game_over(snake *snake1, snake_pos *pos1);
+void set_borders();
+void print_score(int*);
